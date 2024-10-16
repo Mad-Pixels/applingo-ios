a7cfac255438ef8e5231955987774e30a699b78c
mr-chelyshkin <argent-keel-0z@icloud.com>
2024-10-16 11:46:34 +0300

    draft

---
+++ lingocards/Models/ApiRequest.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+struct DictionaryQueryRequest: Codable {
+    var id: String?
+    var name: String?
+    var categoryMain: String?
+    var categorySub: String?
+    var author: String?
+    var isPublic: Bool?
+    var code: String?
+    var lastEvaluated: String?
+
+    init(id: String? = nil, name: String? = nil, categoryMain: String? = nil, categorySub: String? = nil, author: String? = nil, isPublic: Bool? = nil, code: String? = nil, lastEvaluated: String? = nil) {
+        self.id = id
+        self.name = name
+        self.categoryMain = categoryMain
+        self.categorySub = categorySub
+        self.author = author
+        self.isPublic = isPublic
+        self.code = code
+        self.lastEvaluated = lastEvaluated
+    }
+}
