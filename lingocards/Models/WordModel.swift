cfe26044af8fe1914e607e73e44be93043c551b6
mr-chelyshkin <argent-keel-0z@icloud.com>
2024-10-19 14:40:39 +0300

    draft

--- lingocards/Models/WordModel.swift
+++ lingocards/Models/WordModel.swift
@@ -1,44 +1,53 @@
 import Foundation
 import GRDB
 
+/// Implemantation of row in Dictionary data table.
 struct WordItem: Identifiable, Codable, Equatable, FetchableRecord, PersistableRecord {
-    var id: Int
-    
+    /// required fileds
     var tableName: String
     var frontText: String
     var backText: String
     
+    /// optional fields
     var description: String?
     var hint: String?
     
+    /// predefined fields
     var createdAt: Int
     var success: Int
     var weight: Int
     var fail: Int
+    var id: Int
 
     init(
         tableName: String,
         frontText: String,
         backText: String,
+        
         description: String? = nil,
         hint: String? = nil,
+        
         success: Int = 0,
         fail: Int = 0,
         weight: Int = 0,
         id: Int = 0
     ) {
         self.tableName = tableName
+        
         self.frontText = Escape.text(frontText)
         self.backText = Escape.text(backText)
         self.description = description.map { Escape.text($0) } ?? ""
         self.hint = hint.map { Escape.text($0) } ?? ""
+        
         self.success = success
         self.fail = fail
         self.weight = weight
         self.id = id
+        
         self.createdAt = Int(Date().timeIntervalSince1970)
     }
     
+    /// initialize empty object.
     static func empty() -> WordItem {
         return WordItem(
             tableName: "",
@@ -52,15 +61,18 @@ struct WordItem: Identifiable, Codable, Equatable, FetchableRecord, PersistableR
     static func createTable(in db: Database, tableName: String) throws {
         try db.create(table: tableName) { t in
             t.autoIncrementedPrimaryKey("id")
+            
             t.column("tableName", .text).notNull()
             t.column("frontText", .text).notNull()
             t.column("backText", .text).notNull()
-            t.column("description", .text)
-            t.column("hint", .text)
+            
             t.column("createdAt", .integer).notNull()
             t.column("success", .integer).notNull()
             t.column("weight", .integer).notNull()
             t.column("fail", .integer).notNull()
+            
+            t.column("description", .text)
+            t.column("hint", .text)
         }
     }
     
