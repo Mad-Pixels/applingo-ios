f5a56a438d47ea5ee09c923d48d6e507b1485dac
mr-chelyshkin <argent-keel-0z@icloud.com>
2024-11-18 11:35:24 +0300

    draft

--- lingocards/ViewComponents/CompWordRowView.swift
+++ lingocards/ViewComponents/CompWordRowView.swift
@@ -10,9 +10,15 @@ struct CompWordRowView: View {
                 .foregroundColor(ThemeManager.shared.currentThemeStyle.baseTextColor)
                 .frame(maxWidth: .infinity, alignment: .leading)
                 .modifier(BaseTextStyle())
-            Image(systemName: "arrow.left.and.right")
-                .frame(maxWidth: .infinity, alignment: .center)
-                .modifier(MainIconStyle())
+            ZStack {
+                Capsule()
+                    .fill(ThemeManager.shared.currentThemeStyle.accentColor.opacity(0.1))
+                    .frame(width: 36, height: 24)
+                
+                Image(systemName: "arrow.left.and.right")
+                    .font(.system(size: 12, weight: .medium))
+                    .foregroundColor(ThemeManager.shared.currentThemeStyle.accentColor)
+            }
             Text(word.backText)
                 .font(.subheadline)
                 .foregroundColor(ThemeManager.shared.currentThemeStyle.secondaryTextColor)
