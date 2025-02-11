59b0b432b507563f9ffff200d702545f4b87d3d4
mr-chelyshkin <argent-keel-0z@icloud.com>
2025-02-11 18:19:21 +0200

    draft

--- applingo/Sources/UI/Screen/WordList/Views/WordListViewSearch.swift
+++ applingo/Sources/UI/Screen/WordList/Views/WordListViewSearch.swift
@@ -2,12 +2,12 @@ import SwiftUI
 
 /// A view that provides a search bar for filtering words.
 struct WordListViewSearch: View {
-    
     // MARK: - Properties
+    @EnvironmentObject private var themeManager: ThemeManager
+    private let locale: WordListLocale
     
     /// Binding to the search text.
     @Binding var searchText: String
-    private let locale: WordListLocale
     
     // MARK: - Initializer
     
@@ -15,21 +15,17 @@ struct WordListViewSearch: View {
     /// - Parameters:
     ///   - searchText: Binding to the search query.
     ///   - locale: Localization object.
-    init(
-        searchText: Binding<String>,
-        locale: WordListLocale
-    ) {
+    init(searchText: Binding<String>, locale: WordListLocale) {
         self._searchText = searchText
         self.locale = locale
     }
     
     // MARK: - Body
-    
     var body: some View {
         InputSearch(
             text: $searchText,
             placeholder: locale.screenSearch,
-            style: .themed(ThemeManager.shared.currentThemeStyle)
+            style: .themed(themeManager.currentThemeStyle)
         )
     }
 }
