92e4b8b56823f69602a3414687ac9416648caa20
mr-chelyshkin <argent-keel-0z@icloud.com>
2025-03-31 09:29:11 +0300

    [game][quiz] refactor

---
+++ applingo/Sources/Backend/Game/Quiz/Internal/QuizValidation.swift
@@ -0,0 +1,27 @@
+import SwiftUI
+
+internal final class QuizValidation: BaseGameValidation {
+    private var currentCard: QuizModelCard?
+    private var currentWord: DatabaseModelWord?
+    
+    func setCurrentCard(currentCard: QuizModelCard, currentWord: DatabaseModelWord) {
+        self.currentCard = currentCard
+        self.currentWord = currentWord
+    }
+    
+    override func validateAnswer(answer: Any) -> GameValidationResult {
+        guard let answer = answer as? String,
+              let card = currentCard else {
+            return .incorrect
+        }
+        return answer == card.answer ? .correct : .incorrect
+    }
+    
+    override func getCurrentWord() -> DatabaseModelWord? {
+        return currentWord
+    }
+    
+    override func getCorrectAnswer() -> String? {
+        return currentCard?.answer
+    }
+}
--- applingo/Sources/Backend/Game/Quiz/Models/QuizModelCard.swift
+++ applingo/Sources/Backend/Game/Quiz/Models/QuizModelCard.swift
@@ -1,18 +1,15 @@
 import SwiftUI
 
-/// A model representing a quiz card that holds the question, correct answer, and answer options.
 struct QuizModelCard {
     let word: DatabaseModelWord
-    /// The text of the question (either the front text or the back text).
+    
     let question: String
-    /// The correct answer (the opposite text of the question).
     let answer: String
-    /// An array of answer options.
     let options: [String]
-    /// A flag indicating whether the front text is shown as the question.
+    
     let showingFront: Bool
     
-    /// Initializes a new instance of QuizModelCard.
+    /// Initializes the QuizModelCard.
     /// - Parameters:
     ///   - correctWord: The DatabaseModelWord used as the basis for the card.
     ///   - allWords: An array of DatabaseModelWord objects used to extract answer options.
