946bb4867c0a349a31cefc5812287314a82a1f0a
mr-chelyshkin <argent-keel-0z@icloud.com>
2025-04-07 09:16:01 +0300

    draft

--- applingo/Sources/Game/Swipe/Internal/SwipeSettings.swift
+++ applingo/Sources/Game/Swipe/Internal/SwipeSettings.swift
@@ -1,9 +1,44 @@
 import SwiftUI
 
 class SwipeSettings: GameSettings {
+    private(set) var timeDurationSetting: GameSettingItemTime
+    private(set) var countLivesSetting: GameSettingItemInt
     
     /// Initialize the Swipe settings
     init() {
-        super.init(settings: [])
+        countLivesSetting = GameSettingItemInt(
+            id: "countLives",
+            name: LocaleManager.shared.localizedString(for: "settings.game.lives"),
+            defaultValue: AppStorage.shared.gameLives,
+            range: DEFAULT_SURVIVAL_LIVES_MIN...DEFAULT_SURVIVAL_LIVES_MAX,
+            onChange: { newValue in
+                AppStorage.shared.gameLives = newValue
+            }
+        )
+        
+        timeDurationSetting = GameSettingItemTime(
+            id: "timeDuration",
+            name: LocaleManager.shared.localizedString(for: "settings.game.timeDuration"),
+            defaultValue: AppStorage.shared.gameDuration,
+            range: DEFAULT_TIME_DURATION_MIN...DEFAULT_TIME_DURATION_MAX,
+            onChange: { newValue in
+                AppStorage.shared.gameDuration = newValue
+            }
+        )
+        
+        super.init(settings: [
+            timeDurationSetting,
+            countLivesSetting
+        ])
+    }
+    
+    var gameLives: Int {
+        get { getValue(id: "countLives") as? Int ?? DEFAULT_SURVIVAL_LIVES_MIN }
+        set { setValue(id: "countLives", value: newValue) }
+    }
+
+    var gameDuration: TimeInterval {
+        get { getValue(id: "timeDuration") as? TimeInterval ?? DEFAULT_TIME_DURATION_MIN }
+        set { setValue(id: "timeDuration", value: newValue) }
     }
 }
